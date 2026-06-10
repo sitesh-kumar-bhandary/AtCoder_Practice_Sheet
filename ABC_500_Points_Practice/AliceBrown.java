66adb53b421bc4a5d616ff36ebfcba6230140b00
Sitesh01 <siteshisunique1998@gmail.com>
2026-06-10 17:40:58 +0530

    8th - Added some more questions on 300 and 500 points

---
+++ ABC_500_Points_Practice/AliceBrown.java
@@ -0,0 +1,19 @@
+package ABC_500_Points_Practice;
+import java.io.*;
+
+public class AliceBrown {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] exp = br.readLine().split(" ");;
+        long x = Long.parseLong(exp[0]);
+        long y = Long.parseLong(exp[1]);
+
+        if(Math.abs(x-y) <= 1)
+            System.out.println("Brown");
+
+        else
+            System.out.println("Alice");
+            
+        br.close();
+    }
+}
