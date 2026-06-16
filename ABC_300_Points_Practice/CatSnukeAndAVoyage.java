6a9d7fc5b2a34a59a6869822d3f20af7f0f69842
Sitesh01 <siteshisunique1998@gmail.com>
2026-06-16 12:53:25 +0530

    13th - Added some more questions of 300 points

---
+++ ABC_300_Points_Practice/CatSnukeAndAVoyage.java
@@ -0,0 +1,40 @@
+package ABC_300_Points_Practice;
+import java.io.*;
+import java.util.*;
+
+public class CatSnukeAndAVoyage {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] exp = br.readLine().split(" ");
+        int n = Integer.parseInt(exp[0]);
+        int m = Integer.parseInt(exp[1]);
+
+        HashSet<String> set = new HashSet<>();
+        for(int i=0;i<m;i++){
+            String[] exp1 = br.readLine().split(" ");
+            int a = Integer.parseInt(exp1[0]);
+            int b = Integer.parseInt(exp1[1]);
+
+            String key1 = a + "#" + b;
+            String key2 = b + "#" + a;
+            set.add(key1);
+            set.add(key2);
+        }
+
+        String result = catSnukeAndAVoyage(n, m, set);
+        System.out.println(result);
+        br.close();
+    }
+
+    private static String catSnukeAndAVoyage(int n, int m, HashSet<String> set){
+        for(int i=2;i<n;i++){
+            String key1 = 1 + "#" + i;
+            String key2 = i + "#" + n;
+
+            if(set.contains(key1) && set.contains(key2))
+                return "POSSIBLE";
+        }
+
+        return "IMPOSSIBLE";
+    }
+}
